e9882ea8f9b43cddcb9a36274ae08cb88a1c7b99
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2023-10-13 00:42:39 -0300

    export fix

--- auxiliary.go
+++ auxiliary.go
@@ -326,6 +326,7 @@ func GenerateReport(repoName string, branchName string, triggerType string, comm
 		fmt.Println("Error inlining CSS:", err)
 		return "", err
 	}
+	inlinedHtml = strings.Replace(inlinedHtml, "\n", "", -1)
 	fmt.Println(inlinedHtml)
 
 	vars := map[string]string{
